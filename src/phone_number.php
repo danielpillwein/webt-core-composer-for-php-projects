8463a8562ed9b94d2c27783f6c3e355a63d7cc17
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2023-02-22 12:17:11 +0100

    html and css

--- src/phone_number.php
+++ src/phone_number.php
@@ -32,13 +32,20 @@ $result = Builder::create()
     ->build();
 
 ?>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>PAA</title>
     <link rel="stylesheet" href="../style/style.css">
     <style>
+        .wrapper {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
         a {
+            height: 2em;
             color: #EFEEEF;
             background: #0088DF;
             margin: 1em;
@@ -55,21 +62,33 @@ $result = Builder::create()
             fill: #EFEEEF;
             height: 1.5em;
         }
+
+        @media (max-width: 650px) {
+            .wrapper {
+                flex-direction: column;
+            }
+
+            #qr-code {
+                width: 90%;
+            }
+        }
     </style>
 </head>
 <body>
-<a href="../">
-    Go Back
-    <svg viewBox="0 0 48 48">
-        <path d="M14 38v-3h14.45q3.5 0 6.025-2.325Q37 30.35 37 26.9t-2.525-5.775Q31.95 18.8 28.45 18.8H13.7l5.7 5.7-2.1 2.1L8 17.3 17.3 8l2.1 2.1-5.7 5.7h14.7q4.75 0 8.175 3.2Q40 22.2 40 26.9t-3.425 7.9Q33.15 38 28.4 38Z"/>
-    </svg>
-</a>
-<img src="<?php echo $result->getDataUri() ?>">
-<a href="<?php echo $result->getDataUri() ?>" download>
-    Download
-    <svg viewBox="0 0 48 48">
-        <path d="M11 40q-1.2 0-2.1-.9Q8 38.2 8 37v-7.15h3V37h26v-7.15h3V37q0 1.2-.9 2.1-.9.9-2.1.9Zm13-7.65-9.65-9.65 2.15-2.15 6 6V8h3v18.55l6-6 2.15 2.15Z"/>
-    </svg>
-</a>
+<div class="wrapper">
+    <a href="../">
+        Go Back
+        <svg viewBox="0 0 48 48">
+            <path d="M14 38v-3h14.45q3.5 0 6.025-2.325Q37 30.35 37 26.9t-2.525-5.775Q31.95 18.8 28.45 18.8H13.7l5.7 5.7-2.1 2.1L8 17.3 17.3 8l2.1 2.1-5.7 5.7h14.7q4.75 0 8.175 3.2Q40 22.2 40 26.9t-3.425 7.9Q33.15 38 28.4 38Z"/>
+        </svg>
+    </a>
+    <img src="<?php echo $result->getDataUri() ?>" id="qr-code" alt="Phone number QR-Code">
+    <a href="<?php echo $result->getDataUri() ?>" download>
+        Download
+        <svg viewBox="0 0 48 48">
+            <path d="M11 40q-1.2 0-2.1-.9Q8 38.2 8 37v-7.15h3V37h26v-7.15h3V37q0 1.2-.9 2.1-.9.9-2.1.9Zm13-7.65-9.65-9.65 2.15-2.15 6 6V8h3v18.55l6-6 2.15 2.15Z"/>
+        </svg>
+    </a>
+</div>
 </body>
 </html>
